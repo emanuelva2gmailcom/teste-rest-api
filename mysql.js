45497d30d83bb9eccd483cb6cc10c3d0264f838b
Emanuel Vasconcelos Nobre <emanuelvanobre@gmail.com>
2021-06-01 22:03:35 -0300

    modificando o mysql.js

--- mysql.js
+++ mysql.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 exports.pool = mysql.createPool({
-    "user": "root",
-    "password": "QWE!@#45",
-    "database": "ecommerce",
-    "host": "localhost",
-    "port": 3306,
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
 });
 
 /*var pool = mysql.createPool({
