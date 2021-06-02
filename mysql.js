e7643d36a2107ff174c9ed923e3b0a7cb23de124
Emanuel Vasconcelos Nobre <emanuelvanobre@gmail.com>
2021-06-02 11:44:23 -0300

    .

--- mysql.js
+++ mysql.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql')
 
-exports.pool = mysql.createPool({
+/*exports.pool = mysql.createPool({
     "user": "root",
     "password": "QWE!@#45",
     "database": "ecommerce",
     "host": "localhost",
     "port": 3306,
-});
+});*/
 
-/*var pool = mysql.createPool({
+exports.pool = mysql.createPool({
     "user": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
     "host": process.env.MYSQL_HOST,
     "port": process.env.MYSQL_PORT
-})*/
+})
